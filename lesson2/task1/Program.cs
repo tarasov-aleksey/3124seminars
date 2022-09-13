0f4a389b7205ca48b9d53905ad2191852ffa0050
tarasov aleksey <tarasov_aleksey_1994@mail.ru>
2022-09-14 02:03:13 +0400

    первое задание решено

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 100 / 10;
+Console.WriteLine(digit);
---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 100 / 100;
+Console.WriteLine(digit);
