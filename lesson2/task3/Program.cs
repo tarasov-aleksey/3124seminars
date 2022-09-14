c5acbd74fa182c8f86e5ebff5c8d30d2b9aa5b40
tarasov aleksey <tarasov_aleksey_1994@mail.ru>
2022-09-14 17:59:08 +0400

    решена задача 15

--- lesson2/task3/Program.cs
+++ lesson2/task3/Program.cs
@@ -6,3 +6,19 @@
 
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+Console.WriteLine("нет");
+else if (number == 2)
+Console.WriteLine("нет");
+else if (number == 3)
+Console.WriteLine("нет");
+else if (number == 4)
+Console.WriteLine("нет");
+else if (number == 5)
+Console.WriteLine("нет");
+else if (number == 6)
+Console.WriteLine("да");
+else if (number == 7)
+Console.WriteLine("да");
+else
+Console.WriteLine("в неделе всего 7 дней, увы(");
