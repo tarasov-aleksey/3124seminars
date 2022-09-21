1738a708d0b635cc4072dae82da18b33c09ce05b
tarasov aleksey <tarasov_aleksey_1994@mail.ru>
2022-09-21 20:28:35 +0400

    Решение 35 задачи

---
+++ lesson5/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. Найдите количество элементов массива,
+// значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int N=123;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+a[i]=random.Next(0,150);
+for(int i=0;i<a.Length;i++)
+System.Console.Write($"{a[i],4}");
+
+int count=0;
+for(var i=0; i<a.Length;i++)
+{
+if(a[i]>10 && a[i]<99)
+count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count );
