973c83579353eb8009eabfc1d3569dd5ec01c0ab
tarasov aleksey <tarasov_aleksey_1994@mail.ru>
2022-09-27 21:59:10 +0400

    Задание 50 решено

--- lesson7/task2/Program.cs
+++ lesson7/task2/Program.cs
@@ -6,43 +6,45 @@
 // 8 4 2 4
 // 1 7 -> такого числа в массиве нет
 
-while (true)
-{
-int rows = ReadInt("Введите индекс строки: ");
-int colums = ReadInt("Введите индекс столбца: ");
-int[,] numbers = new int[4, 4];
-FillArray2D(numbers);
-PrintArray2D(numbers);
-if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
-else Console.WriteLine($"{rows} {colums} -> такого числа в массиве нет");
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [4,4];
+FillArrayRandomNumbers(numbers);
 
-void FillArray2D(int[,] array)
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 10);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
+        Console.Write(" ");
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
-        }
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("");
+        Console.WriteLine(""); 
     }
-}
-void PrintArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-string s = Console.ReadLine();
-if (s == "stop") break;
 }
