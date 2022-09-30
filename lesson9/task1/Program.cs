55cc7019e279de7a43a386fa6ecb713b8f43362c
tarasov aleksey <tarasov_aleksey_1994@mail.ru>
2022-09-30 11:03:19 +0400

    Задача 64

---
+++ lesson9/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int a = InputNumbers("Введите положительное число: ");
+int count = 2;
+PrintNumber(a, count);
+Console.Write(1);
+if (a < 1)
+{
+ Console.WriteLine($"\nВвели не положительное число: {a}");
+}
+
+void PrintNumber(int a, int count)
+{
+  if (count > a) return;
+  PrintNumber(a, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+Console.WriteLine();
